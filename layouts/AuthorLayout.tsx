37b50b5c2df46f8c1a8ff447c516cad258c09561
Alisher Ortiqov <mcpeblocker@gmail.com>
2022-08-31 15:35:03 +0500

    customized about page

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -17,14 +17,14 @@ export default function AuthorLayout({ children, content }: Props) {
       <div className="pt-8">
         <div className="flex flex-col-reverse items-center justify-between mb-8 sm:flex-row sm:items-center">
           <div className="text-center sm:text-left">
-            <h1 className="text-xl font-bold md:text-3xl lg:text-4xl">Dale Larroder</h1>
+            <h1 className="text-xl font-bold md:text-3xl lg:text-4xl">Alisher Ortiqov</h1>
             <h2 className="text-sm font-normal md:text-base">
               {occupation} <span className="font-semibold">{company}</span>
             </h2>
           </div>
           <div>
             <Image
-              alt="Dale Larroder"
+              alt="Alisher Ortiqov"
               height={130}
               width={130}
               src={avatar || ''}
