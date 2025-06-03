98c706329bbc51e81ef576ff18b157cbba812da4
Alisher Ortiqov <mcpeblocker@gmail.com>
2025-06-03 21:08:40 +0900

    upd: projects & about-me

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,10 +1,17 @@
 const projectsData = [
   {
-    title: 'shashka.uz',
-    description: `Can you beat your friends in checkers? If you think you can, then give this game a try!`,
+    title: 'shashka-uz',
+    description: `You think you can beat others in checkers? This beloved online multiplayer game project is where I learn new tech and improve my skills - e.g. React, Node.js, Socket.io, and more.`,
     imgSrc: '/static/images/project/shashka.png',
     tag: ['javascript', 'css', 'reactjs', 'nodejs', 'socket.io'],
-    href: 'https://play.shashka.uz/',
+    href: '',
+  },
+  {
+    title: 'teambl',
+    description: `Trust Based Project Social Network.`,
+    imgSrc: '/static/images/project/teambl-logo.webp',
+    tag: ['typescript', 'react-native', 'reactjs', 'python', 'django'],
+    href: 'https://teambl.net',
   },
 ]
 
